956e2c062cf94b9f8bfc11a28b6b77372bc8b7e4
ijeomaemeruwa <helloijeoma@gmail.com>
2020-12-15 16:00:21 +0000

    added appropraite data types to users and post models

--- models/user.js
+++ models/user.js
@@ -2,18 +2,27 @@ const mongoose = require("mongoose")
 
 const userSchema = mongoose.Schema({
     userName:{
-        
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
     },
     email: {
-          
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+        lowercase: true
     },
     password: {
-        
+        type: String,
+        trim: true,
+        required: true
     },
     
 },{
     timestamps: true
 })
 
-//added stuff here
+
 module.exports = mongoose.model("User", userSchema)
